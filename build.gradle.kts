a015d7d2b8ca376a547d6e5397dc75193965819f
JOO200 <github@joo200.de>
2023-03-23 15:48:58 +0100

    update dependencies, fix outdated mojang api calls (#19)
    * dependencies: updated dependencies, fix dependencies issues
    
    * updated jdk to java 17
    * updated to paper 1.19.3
    * move different dependencies from implementation to compileOnly/implementationOnly
    * updated dependencies used by paper to the same version
    
    * http-service: fix outdated mojang api calls
    
    Endpoint https://api.mojang.com/user/profiles/<uuid>/names has been removed and is now replaced by https://sessionserver.mojang.com/session/minecraft/profile/<uuid>
    
    * gradle: updated gradle to 8.0.2
    
    * ci: set travis to openjdk17
    
    * fix: set http repository url to minecraft instead of Minecraft

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,31 +28,35 @@ logger.lifecycle("""
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
     maven { url = uri("https://maven.enginehub.org/repo/") }
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 configurations.all {
     resolutionStrategy {
-        force("com.google.guava:guava:21.0")
+        force("com.google.guava:guava:${Versions.GUAVA}")
     }
 }
 
 dependencies {
-    "implementation"("com.google.guava:guava:${Versions.GUAVA}")
-    "implementation"("com.google.code.findbugs:jsr305:1.3.9")
-    "implementation"("com.googlecode.json-simple:json-simple:1.1")
-    "implementation"("org.xerial:sqlite-jdbc:3.7.2")
-    "implementation"("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
+    "compileOnly"("com.google.guava:guava:${Versions.GUAVA}")
+    "compileOnly"("com.google.code.findbugs:jsr305:1.3.9")
+    "compileOnly"("org.xerial:sqlite-jdbc:3.36.0.3")
+    "compileOnly"("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 
-    "testImplementation"("org.junit.jupiter:junit-jupiter-api:${Versions.JUNIT}")
-    "testImplementation"("org.junit.jupiter:junit-jupiter-params:${Versions.JUNIT}")
+    "testImplementation"("com.google.guava:guava:${Versions.GUAVA}")
+    "testImplementation"("com.google.code.findbugs:jsr305:1.3.9")
+    "testImplementation"("org.xerial:sqlite-jdbc:3.36.0.3")
+    "testImplementation"("com.googlecode.json-simple:json-simple:1.1")
+    "testImplementation"("junit:junit:${Versions.JUNIT}")
+    "testImplementation"("org.junit.jupiter:junit-jupiter-api:${Versions.JUPITER}")
+    "testImplementation"("org.junit.jupiter:junit-jupiter-params:${Versions.JUPITER}")
     "testImplementation"("org.hamcrest:hamcrest:2.2")
-    "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${Versions.JUNIT}")
+    "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${Versions.JUPITER}")
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     withSourcesJar()
     withJavadocJar()
 }
